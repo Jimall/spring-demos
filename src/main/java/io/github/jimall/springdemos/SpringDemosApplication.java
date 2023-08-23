a514a740ffff87bba4ed5210a55a54bf38adbb3c
Jimall <jimall0616@outlook.com>
2023-08-23 11:11:24 +0800

    Initial commit

---
+++ src/main/java/io/github/jimall/springdemos/SpringDemosApplication.java
@@ -0,0 +1,13 @@
+package io.github.jimall.springdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jimall/springdemos/SpringDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jimall.springdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
